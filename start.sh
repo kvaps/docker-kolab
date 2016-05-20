df70363a5fca147b9c42c98c8d01a8206600e5ec
kvaps <kvapss@gmail.com>
2016-05-20 19:15:09 +0300

    add: default.env config

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 chk_env() {
-    eval env=\$$1
-    val=${env:-$2}
-    if [ -z $val ]; then
+    eval env="\$$1"
+    val="${env:-$2}"
+    if [ -z "$val" ]; then
         echo "err:  Enviroment vaiable \$$1 is not set."
         exit 1
     fi  
@@ -12,102 +12,20 @@ chk_env() {
 
 load_defaults() {
 
-    # Options
-    chk_env  WEBSERVER             "nginx"
-    chk_env  FORCE_HTTPS           true
-    chk_env  NGINX_CACHE           false
-    chk_env  SPAM_SIEVE            true
-    chk_env  SPAM_SIEVE_TIMEOUT    "15m"
-    chk_env  FAIL2BAN              true
-    chk_env  DKIM                  true
-    chk_env  CERT_PATH             "/etc/pki/tls/kolab"
+    defaults_file="defaults.env"
+    prefix="DEFAULT_"
 
-    # Sizes
-    chk_env  MAX_MEMORY_SIZE       "256M"
-    chk_env  MAX_FILE_SIZE         "30M"
-    chk_env  MAX_MAIL_SIZE         "30M"
-    chk_env  MAX_MAILBOX_SIZE      "50M"
-    chk_env  MAX_BODY_SIZE         "50M"
+    # Export environment variables from $defaults_file with $prefix
+    eval export $(cat $defaults_file | sed -e '/^\#/d' -e '/^$/d' -e 's/^/ /' | tr '\n' ' ' | sed -E 's/ ([a-z2A-Z0-9_]+=\S)/ '$prefix'\1/g')
 
-    # Services
-    chk_env SERVICE_RSYSLOG         true
-    chk_env SERVICE_HTTPD           true
-    chk_env SERVICE_NGINX           false
-    chk_env SERVICE_PHP_FPM         false
-    chk_env SERVICE_MYSQLD          true
-    chk_env SERVICE_DIRSRV          true
-    chk_env SERVICE_POSTFIX         true
-    chk_env SERVICE_CYRUS_IMAPD     true
-    chk_env SERVICE_AMAVISD         true
-    chk_env SERVICE_CLAMD           true
-    chk_env SERVICE_WALLACE         true
-    chk_env SERVICE_KOLABD          true
-    chk_env SERVICE_KOLAB_SASLAUTHD true
-    chk_env SERVICE_OPENDKIM        false
-    chk_env SERVICE_FAIL2BAN        false
-    chk_env SERVICE_SET_SPAM_SIEVE  false
+    # Getting loaded envs
+    envs=$(env | grep $prefix | cut -d= -f1 | cut -c $[${#prefix}+1]- | grep -v SERVICE_)
 
-    volumes=(config data log)
+    # Checkinkg envs
+    for env in ${envs[@]}; do
+        eval 'chk_env "'$env'" "$'${prefix}${env}'"'
+    done
 
-    config_dirs=(
-        /etc/dirsrv
-        /etc/fail2ban
-        /etc/httpd
-        /etc/my.cnf
-        /etc/cyrus.conf
-        /etc/imapd.conf
-        /etc/imapd.annotations.conf
-        /etc/kolab
-        /etc/kolab-freebusy
-        /etc/nginx
-        /etc/opendkim
-        /etc/opendkim.conf
-        /etc/php-fpm.d
-        /etc/php-fpm.conf
-        /etc/php.d
-        /etc/php.ini
-        /etc/postfix
-        /etc/roundcubemail
-        /etc/sasldb2
-        /etc/supervisord.conf
-        /etc/clamd.conf
-        /etc/clamd.d
-        /etc/freshclam.conf
-        /etc/iRony
-        /etc/ssl
-        /etc/mailname
-        /etc/mail
-        /etc/pki
-    )
-    
-    data_dirs=(
-        /var/lib/mysql
-        /var/lib/dirsrv
-        /var/lib/imap
-        /var/lib/nginx
-        /var/lib/spamassassin
-        /var/lib/clamav
-        /var/spool
-    )
-    
-    log_dirs=(
-        /var/log/chwala
-        /var/log/clamav
-        /var/log/dirsrv
-        /var/log/httpd
-        /var/log/iRony
-        /var/log/kolab
-        /var/log/kolab-freebusy
-        /var/log/kolab-syncroton
-        /var/log/kolab-webadmin
-        /var/log/maillog
-        /var/log/messages
-        /var/log/mysqld.log
-        /var/log/nginx
-        /var/log/php-fpm
-        /var/log/roundcubemail
-        /var/log/supervisor
-    )
 }
 
 print_spaces() {
@@ -115,7 +33,7 @@ print_spaces() {
     eval 'printf "%0.s " {1..'$spaces_count'}'
 }
 
-chk_dirs() {
+map_dirs() {
 
     echo "Processing folders:"
     echo "STORAGE   FOLDER                   ACTION"
