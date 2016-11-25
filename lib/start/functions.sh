1354e2cf9b6f7d0ee0094baccb8e61b45f2f3fd5
kvaps <kvapss@gmail.com>
2016-11-26 02:45:16 +0300

    fix: move storage hint

--- lib/start/functions.sh
+++ lib/start/functions.sh
@@ -59,10 +59,12 @@ function detect_old_image {
         >&2 echo
         >&2 echo "Please move your data directories to separate storages like:"
         >&2 echo
-        >&2 echo "    # mv ./data/etc ./config"
-        >&2 echo "    # mv ./data/var/lib ./data"
-        >&2 echo "    # mv ./data/var/log ./log"
-        >&2 echo "    # mv ./data/var/spool ./spool"
+        >&2 echo "    # shopt -s dotglob"
+        >&2 echo "    # mv ./data/etc/* ./config/"
+        >&2 echo "    # mv ./data/var/lib/* ./data/"
+        >&2 echo "    # mv ./data/var/log/* ./log/"
+        >&2 echo "    # mv ./data/var/spool/* ./spool/"
+        >&2 echo "    # rmdir ./data/etc/ ./data/var/* ./data/var/"
         >&2 echo
         >&2 echo "After it, don't forget to create version.conf file:"
         >&2 echo
